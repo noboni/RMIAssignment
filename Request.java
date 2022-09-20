5d38beab3b1dcc2f91f9672103a19f47d53a8887
noboni <noboni5199@gmail.com>
2022-09-20 17:59:22 +0200

    created the request object

--- Request.java
+++ Request.java
@@ -1,2 +1,31 @@
+import java.util.List;
+
 public class Request {
+    private String methodName;
+    private List<String> parameters;
+    private Long zone;
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public List<String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public Long getZone() {
+        return zone;
+    }
+
+    public void setZone(Long zone) {
+        this.zone = zone;
+    }
 }
