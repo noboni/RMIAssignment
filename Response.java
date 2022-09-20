0ff69ecbeedea53a2ce4f9af29de68fa0d4280de
DESKTOP-KALUK8C\meghl <meghla515@gmail.com>
2022-09-20 20:37:56 +0200

    Response properties added.

--- Response.java
+++ Response.java
@@ -1,2 +1,66 @@
+import java.util.List;
 public class Response {
+    private Long result;
+    private String methodName;
+    private List<String> parameters;
+    private String turnAroundTime;
+    private String executionTime;
+    private String waitingTime;
+    private String serverId;
+
+    public long getResult() {
+        return result;
+    }
+
+    public void setResult(long result) {
+        this.result = result;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public List<String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public String getTurnAroundTime() {
+        return turnAroundTime;
+    }
+
+    public void setTurnAroundTime(String turnAroundTime) {
+        this.turnAroundTime = turnAroundTime;
+    }
+
+    public String getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(String executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    public String getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(String waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public String getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(String serverId) {
+        this.serverId = serverId;
+    }
 }
