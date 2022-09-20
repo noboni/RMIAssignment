77a88309c7e762baa69dca4acbd04bf0842f7bf8
DESKTOP-KALUK8C\meghl <meghla515@gmail.com>
2022-09-20 19:53:47 +0200

    Client input file parse

--- Client.java
+++ Client.java
@@ -17,4 +17,46 @@ public class Client {
 
         }catch(Exception e){System.out.println(e);}
     }
+
+    public static List<Request> getAllMethodInformation() {
+        List<Request> result = new ArrayList<Request>();
+        String fileName = "2022-09-07-input.txt";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+            try {
+                // Read first line
+                String line = br.readLine();
+                // Make sure file has correct headers
+                if (line == null) throw new IllegalArgumentException("File is empty");
+                
+                while ((line = br.readLine()) != null) {
+                    // Break line into entries using space
+                    String[] items = line.split(" ");
+                    try {
+                        // Convert data to request record
+                        Request request = new Request();
+                        request.setMethodName(items[0]);
+                        List<String> params = new ArrayList<String>();
+                        for(int i = 1; i < items.length - 1; i++){
+                            params.add(items[i]);
+                        }
+                        request.setParameters(params);
+                        request.setZone(Long.parseLong(items[items.length - 1].split(":")[1]));
+                        result.add(request);
+                        System.out.println(request.getParameters());
+                    } catch (ArrayIndexOutOfBoundsException | NumberFormatException | NullPointerException e) {
+                        // Caught errors indicate a problem with data format -> Print warning and continue
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Total size---------------" + result.size());
+                return result;
+            } finally {
+                br.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
